234691f7ecdebcf22abd7dd45305295460fac52e
Miguel Arlandy <marlandy@autentia.com>
2014-11-30 10:03:27 +0100

    Added coverage reports

--- karma.conf.js
+++ karma.conf.js
@@ -1,34 +1,38 @@
-module.exports = function(config){
-  config.set({
-
-    basePath : './',
-
-    files : [
-      'app/lib/angular/angular.js',
-      'app/lib/angular-mocks/angular-mocks.js',
-      'app/js/**/*.js',
-      'tests/**/*.js'
-    ],
-
-    autoWatch : true,
-
-    frameworks: ['jasmine'],
-
-    browsers : ['Chrome', 'Firefox'],
-
-    singleRun : true,
-
-    plugins : [
+module.exports = function (config) {
+    config.set({
+        basePath: './',
+        reporters: ['junit', 'coverage'],
+        files: [
+            'app/lib/angular/angular.js',
+            'app/lib/angular-mocks/angular-mocks.js',
+            'app/js/**/*.js',
+            'tests/**/*.js'
+        ],
+        autoWatch: true,
+        frameworks: ['jasmine'],
+        browsers: ['Chrome','Firefox'],
+        singleRun: true,
+        plugins: [
             'karma-chrome-launcher',
             'karma-firefox-launcher',
             'karma-jasmine',
-            'karma-junit-reporter'
-            ],
+            'karma-junit-reporter',
+            'karma-coverage'
+        ],
+        junitReporter: {
+            outputFile: 'test_reports/junit/junit.xml',
+            suite: 'unit'
+        },
+        preprocessors: {
+            'app/js/**/*.js': ['coverage']
+        },
+        coverageReporter: {
+            dir: 'test_reports/coverage/',            
+            reporters: [
+                {type: 'html', subdir: '.'},
+                {type: 'cobertura', subdir: '.', file: 'cobertura.xml'}
+            ]
+        }
 
-    junitReporter : {
-      outputFile: 'test_out/unit.xml',
-      suite: 'unit'
-    }
-
-  });
+    });
 };
